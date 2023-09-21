7cee0e9d5354343713192ce834bf1cd62d821987
Dickson Pang <34560707+dicksonpys@users.noreply.github.com>
2023-09-21 18:16:15 +0800

    boilerplate for devnet

---
+++ src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import "@/styles/globals.css";
+import { UnifiedWalletProvider } from "@jup-ag/wallet-adapter";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <UnifiedWalletProvider
+      wallets={[]}
+      config={{
+        autoConnect: false,
+        env: "mainnet-beta",
+        metadata: {
+          name: "UnifiedWallet",
+          description: "UnifiedWallet",
+          url: "https://jup.ag",
+          iconUrls: ["https://jup.ag/favicon.ico"],
+        },
+        // notificationCallback: WalletNotification,
+        // walletlistExplanation: {
+        //   href: "https://station.jup.ag/docs/additional-topics/wallet-list",
+        // },
+      }}
+    >
+      <Component {...pageProps} />
+    </UnifiedWalletProvider>
+  );
+}
